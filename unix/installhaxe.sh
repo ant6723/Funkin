56879da46a52444a21454d8b36d0ae1be14bec6d
ant6723 <antd39487@gmail.com>
2025-03-21 13:12:39 -0400

    Create installhaxe.sh

---
+++ unix/installhaxe.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:haxe/releases -y
+sudo apt-get update
+sudo apt-get install haxe -y
+mkdir ~/haxelib && haxelib setup ~/haxelib
